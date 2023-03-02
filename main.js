702c81dc3af874c8d11eb7baf4793133448b2f4e
Amar <108946547+AmarVcode@users.noreply.github.com>
2023-03-02 12:47:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -16,14 +16,3 @@ function menu(){
 
 
 
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
-    $('.carousel').carousel();
-  });
