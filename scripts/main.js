0ec80353b619f6ed0c4aedd130960636673e501d
Amar <108946547+AmarVcode@users.noreply.github.com>
2023-03-25 14:09:09 -0400

    created

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+// navbar code
+function show(){
+    let div= document.getElementById("navbar");
+    if(div.classList.contains("shownav")){
+        div.classList.remove("shownav");
+    }
+    else{
+        div.classList.add("shownav");
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+var container = document.querySelector(".swiper-container");
+
+
+
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 1,
+    autoplay: {
+        delay: 3000,
+      },
+    
+  });
