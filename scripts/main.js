390fad029a08126cc3d1bedcd43d88dd84ea4541
Amar <108946547+AmarVcode@users.noreply.github.com>
2023-03-30 11:06:31 -0400

    updated

--- scripts/main.js
+++ scripts/main.js
@@ -30,10 +30,33 @@ var mySwiper = new Swiper('.swiper-container', {
       },
   });
 
+
+
+
+ 
+
+
+// responsive code
+
+let body=document.getElementsByTagName("body")[0];
+body=body.offsetWidth
+
+
+if(body >= 500){
   var mySwiper2 = new Swiper('.swiper-container2', {
-    loop: true,
-    slidesPerView: 1,
-    autoplay: {
-        delay: 3000,
-      },
-  });
+        loop: true,
+        slidesPerView: 2,
+        autoplay: {
+            delay: 3000,
+          },
+      });
+}
+if(body <500){
+    var mySwiper2 = new Swiper('.swiper-container2', {
+        loop: true,
+        slidesPerView: 1,
+        autoplay: {
+            delay: 3000,
+          },
+      });
+}
